8b707937708ead924b7805018356ab65b986df3f
lp306470-prog <lp306470@gmail.com>
2026-04-29 16:34:22 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const axios = require("axios");
 const cors = require("cors");
-
+const crypto = require("crypto");
 const app = express();
 app.use(express.json());
 app.use(cors());
