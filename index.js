0827ddf74c21484324c3328ace64ed5080482ad1
lp306470-prog <lp306470@gmail.com>
2026-04-29 15:36:47 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+const express = require("express");
+const app = express();
 
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Servidor funcionando 🚀");
+});
+
+app.post("/webhook/mercadopago", (req, res) => {
+  console.log("Webhook recebido:", req.body);
+  res.sendStatus(200);
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Rodando na porta " + PORT);
+});
